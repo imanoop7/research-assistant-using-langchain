f4acf8e2a0bfb6390c822f72c4c981b585071e52
imanoop7 <64773373+imanoop7@users.noreply.github.com>
2024-04-05 09:12:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 import os
 from langchain_core.runnables import RunnablePassthrough
 from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
+import json
 load_dotenv()
 
 
@@ -57,13 +58,90 @@ chain = RunnablePassthrough.assign(
     context= lambda x:web_scraper(x["url"])[:10000]
 )|prompt|model|StrOutputParser()
 
-output=RunnablePassthrough.assign(
+output_web_chain=RunnablePassthrough.assign(
     urls = lambda x: web_search(x["question"])
 )| (lambda x: [{"question":x["question"], "url":u}for u in x["urls"]])|chain.map()
 
 
 
-response =output.invoke({
-    "question" : "what is langgraph ?"
-})
+# response =output_web_chain.invoke({
+#     "question" : "what is langgraph ?"
+# })
 
+
+SEARCH_PROMPT = ChatPromptTemplate.from_messages(
+    [
+        (
+            "user",
+            "Write 3 google search queries to search online that form an "
+            "objective opinion from the following: {question}\n"
+            "You must respond with a list of strings in the following format: "
+            '["query 1", "query 2", "query 3"].',
+        ),
+    ]
+)
+
+search_question_chain = SEARCH_PROMPT | model | StrOutputParser() | json.loads
+
+full_research_chain = search_question_chain | (lambda x: [{"question": q} for q in x]) | output_web_chain.map()
+
+WRITER_SYSTEM_PROMPT = "You are an AI critical thinker research assistant. Your sole purpose is to write well written, critically acclaimed, objective and structured reports on given text."  # noqa: E501
+
+
+
+RESEARCH_REPORT_TEMPLATE = """Information:
+--------
+{research_summary}
+--------
+Using the above information, answer the following question or topic: "{question}" in a detailed report -- \
+The report should focus on the answer to the question, should be well structured, informative, \
+in depth, with facts and numbers if available and a minimum of 1,200 words.
+You should strive to write the report as long as you can using all relevant and necessary information provided.
+You must write the report with markdown syntax.
+You MUST determine your own concrete and valid opinion based on the given information. Do NOT deter to general and meaningless conclusions.
+Write all used source urls at the end of the report, and make sure to not add duplicated sources, but only one reference for each.
+You must write the report in apa format.
+Please do your best, this is very important to my career."""  # noqa: E501
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", WRITER_SYSTEM_PROMPT),
+        ("user", RESEARCH_REPORT_TEMPLATE),
+    ]
+)
+
+def collapse_list_of_lists(list_of_lists):
+    content = []
+    for l in list_of_lists:
+        content.append("\n\n".join(l))
+    return "\n\n".join(content)
+
+chain_last = RunnablePassthrough.assign(
+    research_summary= full_research_chain | collapse_list_of_lists
+) | prompt | model | StrOutputParser()
+
+
+
+
+#!/usr/bin/env python
+from fastapi import FastAPI
+from langserve import add_routes
+
+
+app = FastAPI(
+  title="LangChain Server",
+  version="1.0",
+  description="A simple api server using Langchain's Runnable interfaces",
+)
+
+add_routes(
+    app,
+    chain_last,
+    path="/research-assistant",
+)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
